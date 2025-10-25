50675ec12982bd0560255334be6de7ed4c02cc23
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2025-10-26 00:04:05 +0300

    feat: added 3d mac model

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,3 +1,11 @@
+import { Mesh, Material } from "three";
+import type { GLTF } from "three-stdlib";
+
+export interface GLTFResult extends GLTF {
+  nodes: Record<string, InstanceType<typeof Mesh>>;
+  materials: Record<string, InstanceType<typeof Material>>;
+}
+
 export type NavLink = {
   label: string;
   href: string;
